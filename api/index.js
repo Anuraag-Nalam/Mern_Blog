918af3dd3d85b445c3e4488f1c0bc325d9c13f05
Anuraag-Nalam <nalamanuraag17@gmail.com>
2024-11-27 23:46:02 -0500

    routes Added

--- api/index.js
+++ api/index.js
@@ -1,13 +1,19 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js'
+
 dotenv.config()
 const app = express()
+
 mongoose.connect(process.env.MONGO).then(() => {
     console.log('Mongo Connected')
 }).catch((err) => {
     console.log("some error")
 })
+
 app.listen(3000, () => {
     console.log("server is running on 3000!")
 })
+
+app.use('/api/user', userRoutes)
