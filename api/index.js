229da108e832c15b5d6b9ab2af9453d2fdb6b164
Anuraag-Nalam <nalamanuraag17@gmail.com>
2024-11-27 20:42:42 -0500

    Mongo Connection

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express()
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Mongo Connected')
+}).catch((err) => {
+    console.log("some error")
+})
 app.listen(3000, () => {
     console.log("server is running on 3000!")
 })
