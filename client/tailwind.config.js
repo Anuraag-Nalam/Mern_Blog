42a71acebbdb09293e76d81045062de14ff7385c
Anuraag-Nalam <nalamanuraag17@gmail.com>
2024-11-27 20:25:10 -0500

    Backend installation

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+const flowbite = require("flowbite-react/tailwind");
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbite.plugin()]
 }
+
+// const flowbite = require("flowbite-react/tailwind");
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     // ...
+//     flowbite.content(),
+//   ],
+//   plugins: [
+//     // ...
+//     flowbite.plugin(),
+//   ],
+// };
