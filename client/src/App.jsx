a553490e980440f671261936f400c81d4a1d0e22
Anuraag-Nalam <nalamanuraag17@gmail.com>
2024-12-22 13:21:53 -0500

    Dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import DashBoard from './pages/DashBoard'
 import Projects from './pages/Projects'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import PrivateRoute from './components/PrivateRoute'
 
 const App = () => {
   return (
@@ -17,8 +18,11 @@ const App = () => {
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<DashBoard />}>
+          </Route>
+        </Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
-        <Route path="/dashboard" element={<DashBoard />}></Route>
         <Route path="/projects" element={<Projects />}></Route>
       </Routes>
       <Footer />
