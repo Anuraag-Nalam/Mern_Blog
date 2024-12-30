f6075d12dfe8d4860dfbe952692705fad8f2b35e
Anuraag-Nalam <nalamanuraag17@gmail.com>
2024-12-30 13:22:07 -0500

    delete post functionality added to dashboard

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -82,3 +82,16 @@ export const getPosts = async (req, res, next) => {
         next(error)
     }
 }
+
+export const deletePosts = async (req, res, next) => {
+    if (!req.user.isAdmin || req.params.userId !== req.user.id) {
+        return next(errorHandler(403, 'You are not allowed to delete the post'))
+    }
+    try {
+        await Post.findByIdAndDelete(req.params.postId)
+        res.status(200).json('The post has been deleted')
+    }
+    catch (error) {
+        next(error)
+    }
+}
