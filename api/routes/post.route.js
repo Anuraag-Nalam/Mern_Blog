fc737c0fd26383b0d3b23f2228e9362952be1edb
Anuraag-Nalam <nalamanuraag17@gmail.com>
2024-12-30 16:09:37 -0500

    udpate post functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { verifyUser } from '../utils/verifyUser.js'
-import { create, getPosts, deletePosts } from '../controllers/post.controller.js'
+import { create, getPosts, deletePosts, updatePost } from '../controllers/post.controller.js'
 
 const router = express.Router()
 router.post('/create', verifyUser, create)
 router.get('/getPosts', getPosts)
 router.delete('/deletePost/:postId/:userId', verifyUser, deletePosts)
+router.put('/updatepost/:postId/:userId', verifyUser, updatePost)
 
 export default router
